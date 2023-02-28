6fbce04a3acb70548e4d0235d8b0a1b038bb51bc
SHASHWAT SHARMA <96010133+shashwat12304@users.noreply.github.com>
2023-02-28 04:36:26 +0000

    Added navigation files to KNN and Missforest, added Missforest.

--- app.py
+++ app.py
@@ -4,14 +4,32 @@ app = Flask(__name__)
 
 TECHNIQUES = [
   {
-    'id': 1,
-    'title': 'KNN Imputation',
-    'description': 'KNN Imputer based on k-Nearest neighbours algorithm.'
+    'id':
+    1,
+    'title':
+    'KNN Imputation',
+    'description':
+    'KNN Imputer based on k-Nearest neighbours algorithm.',
+    'url':
+    'https://imputation-techniques.shashwat12304.repl.co/KNN',
+    'code':
+    'https://github.com/shashwat12304/Imputation-techniques',
+    'mail':
+    'mailto:shashwatsharma12304@gmail.com?subject=Doubt%20regarding%20KNN%20imputer&body=Name%20%3A%20%3Cyour%20name%3E%0D%0AGithub%20Link%3A%20%3Cyour%20repository%20link%3E%0D%0ADescription%3A%20%3CIssues%20to%20be%20removed%3E'
   },
   {
-    'id': 2,
-    'title': 'MissForest',
-    'description': 'A Random Forest algorithm based model imputation approach.'
+    'id':
+    2,
+    'title':
+    'MissForest',
+    'description':
+    'A Random Forest algorithm based model imputation approach.',
+    'url':
+    'https://imputation-techniques.shashwat12304.repl.co/Missforest',
+    'code':
+    'https://github.com/shashwat12304/Imputation-techniques',
+    'mail':
+    'mailto:shashwatsharma12304@gmail.com?subject=Doubt%20regarding%20KNN%20imputer&body=Name%20%3A%20%3Cyour%20name%3E%0D%0AGithub%20Link%3A%20%3Cyour%20repository%20link%3E%0D%0ADescription%3A%20%3CIssues%20to%20be%20removed%3E'
   },
 ]
 
@@ -24,6 +42,11 @@ def hello_jovian():
 @app.route("/KNN")
 def hello_KNN():
   return render_template('KNN.html')
+
+
+@app.route("/Missforest")
+def hello_forest():
+  return render_template('Missforest.html')
 
 
 @app.route("/api/techniques")
